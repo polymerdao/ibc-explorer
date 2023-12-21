c7daf4c999e8cdf336a1c4f901b498e6180f94b2
inkvi <374203+Inkvi@users.noreply.github.com>
2023-12-20 16:53:09 -0800

    Render Base e2e latency

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@
 import React, { useState, useEffect } from 'react';
 import { Card, Metric, Text, Title, BarList, Flex, Grid, Divider } from '@tremor/react';
 import * as stats from 'stats-lite';
-import { fetchEvmData, getLatestBlock } from "./events";
+import { CHAIN, fetchEvmData, getLatestBlock } from "./events";
 import DateTimeRangePicker from "./components/DateTimePicker";
 import { Block } from "ethers";
 
@@ -33,60 +33,60 @@ interface MetricData {
   }[];
 }
 
+const CHAINS: CHAIN[] = ['Optimism', 'Base'];
+
 const MetricsPage: React.FC = () => {
-  const [blockRange, setBlockRange] = useState<[number, number]>([0, 0]);
-  const [data, setData] = useState<MetricData[]>([
-    {
-      category: 'Optimism Tx Latency in seconds',
-      stat: 0,
-      data: [],
-    },
-  ]);
+  const now = new Date();
+  const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0);
+
+  const [dateRange, setDateRange] = useState<[Date, Date]>([startOfDay, now]);
+  const [data, setData] = useState<MetricData[]>([]);
 
   const fetchData = async () => {
-    const txLatencies = await fetchEvmData(...blockRange, 'Optimism');
-    const latencyStats = calculateStats(txLatencies);
+    const newData = await Promise.all(CHAINS.map(async (chain) => {
+      // Assuming block time is 2 seconds
+      const blockTime = 2;
 
-    const latencies = [
-      {name: "Avg", value: latencyStats.avg},
-      {name: "Min", value: latencyStats.min},
-      {name: "Median", value: latencyStats.median},
-    ];
+      const latestBlock = await getLatestBlock(chain);
 
-    setData([
-      {
-        category: 'E2E Tx Latency in seconds',
+      const blockStartNumber = calculateBlockNumber(dateRange[0].getTime() / 1000, latestBlock!, blockTime);
+      const blockEndNumber = calculateBlockNumber(dateRange[1].getTime() / 1000, latestBlock!, blockTime);
+
+      console.log(`Block Number for ${chain} for Start Date: ${blockStartNumber}`);
+      console.log(`Block Number for ${chain} End Date ${blockEndNumber}`);
+
+
+      const txLatencies = await fetchEvmData(blockStartNumber, blockEndNumber, chain);
+      const latencyStats = calculateStats(txLatencies);
+
+      const latencies = [
+        { name: "Avg", value: latencyStats.avg },
+        { name: "Min", value: latencyStats.min },
+        { name: "Median", value: latencyStats.median },
+      ];
+
+      return {
+        category: `${chain} E2E Tx Latency in seconds`,
         stat: txLatencies.length,
         data: latencies,
-      },
-    ]);
+      };
+    }));
+
+    setData(newData);
   };
 
   useEffect(() => {
     fetchData();
-  }, [blockRange]);
+  }, [dateRange]);
 
   const handleRangeChange = async (newStartDate: Date, newEndDate: Date) => {
-    console.log('Range Changed:', newStartDate, 'to', newEndDate);
-
-    // Assuming block time is 2 seconds
-    const blockTime = 2;
-
-    const latestBlock = await getLatestBlock();
-
-    const blockStartNumber = calculateBlockNumber(newStartDate.getTime() / 1000, latestBlock!, blockTime);
-    const blockEndNumber = calculateBlockNumber(newEndDate.getTime() / 1000, latestBlock!, blockTime);
-
-    console.log('Block Number for Start Date:', blockStartNumber);
-    console.log('Block Number for End Date:', blockEndNumber);
-    setBlockRange([blockStartNumber, blockEndNumber]);
+    setDateRange([newStartDate, newEndDate])
   };
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
-
-      <DateTimeRangePicker onRangeChange={handleRangeChange}/>
-      <Divider>Optimism Sepolia</Divider>
+      <DateTimeRangePicker onRangeChange={handleRangeChange} initialStartDate={startOfDay} initialEndDate={now}/>
+      <Divider>Sepolia</Divider>
       <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
         {data.map((item) => (
           <Card key={item.category}>
