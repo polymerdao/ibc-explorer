4b4ccac7752053db99d1dffb3d7ce846ec74d260
inkvi <374203+Inkvi@users.noreply.github.com>
2023-12-21 12:42:59 -0800

    Make block time configurable

--- app/chains.ts
+++ app/chains.ts
@@ -4,18 +4,19 @@ import { ethers } from "ethers";
 export type CHAIN = 'optimism' | 'base';
 
 export const CHAIN_CONFIGS: {
-  [key in CHAIN]: { id: number; rpc: string, dispatcher: string };
+  [key in CHAIN]: { id: number; rpc: string, dispatcher: string, blockTime: number };
 } = {
   optimism: {
     id: 11155420,
     rpc: "https://opt-sepolia.g.alchemy.com/v2/RN7slh_2cUIuzhxo4M9VgYCbqRcPOmkJ",
-    dispatcher: "0xD92B86315CBcf9cC612F0b0542E0bE5871bCa146"
+    dispatcher: "0xD92B86315CBcf9cC612F0b0542E0bE5871bCa146",
+    blockTime: 2,
   },
   base: {
     id: 84532,
     rpc: "https://base-sepolia.g.alchemy.com/v2/zGVxj0T-xvSR29_t7MlIhqRskkSwugVM",
-    dispatcher: "0xab6AEF0311954C40AcD4D1DED56CAAE9cc074975"
-
+    dispatcher: "0xab6AEF0311954C40AcD4D1DED56CAAE9cc074975",
+    blockTime: 2,
   }
 };
 
