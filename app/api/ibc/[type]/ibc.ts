aba23027c8c64ffd2428543390f587a49d16e0c9
inkvi <374203+Inkvi@users.noreply.github.com>
2024-01-09 16:42:45 -0600

    Disable clients page and handle api errors

--- app/api/ibc/[type]/ibc.ts
+++ app/api/ibc/[type]/ibc.ts
@@ -2,37 +2,28 @@ import { execSync } from "child_process";
 import { ChannelsPaginated, ClientStatesPaginated, ConnectionsPaginated } from "../../../schemas";
 
 export async function getConnections(apiUrl: string) {
-  try {
-    const cliOutput = execSync(
-      `polymerd query ibc connection connections --output json --node ${apiUrl}`
-    );
-    const parsedData = ConnectionsPaginated.parse(JSON.parse(cliOutput.toString()));
-    return parsedData.connections
-  } catch (e) {
-    return []
-  }
+  const cliOutput = execSync(
+    `polymerd query ibc connection connections --output json --node ${apiUrl}`,
+    {timeout: 10 * 1000}
+  );
+  const parsedData = ConnectionsPaginated.parse(JSON.parse(cliOutput.toString()));
+  return parsedData.connections
 }
 
 export async function getClients(apiUrl: string) {
-  try {
-    const cliOutput = execSync(
-      `polymerd query ibc client states --output json --node ${apiUrl}`
-    );
-    const parsedData = ClientStatesPaginated.parse(JSON.parse(cliOutput.toString()));
-    return parsedData.client_states
-  } catch (e) {
-    return []
-  }
+  const cliOutput = execSync(
+    `polymerd query ibc client states --output json --node ${apiUrl}`,
+    {timeout: 20 * 1000}
+  );
+  const parsedData = ClientStatesPaginated.parse(JSON.parse(cliOutput.toString()));
+  return parsedData.client_states
 }
 
 export async function getChannels(apiUrl: string) {
-  try {
-    const cliOutput = execSync(
-      `polymerd query ibc channel channels --output json --node ${apiUrl}`
-    );
-    const parsedData = ChannelsPaginated.parse(JSON.parse(cliOutput.toString()));
-    return parsedData.channels
-  } catch (e) {
-    return []
-  }
+  const cliOutput = execSync(
+    `polymerd query ibc channel channels --output json --node ${apiUrl}`,
+    {timeout: 10 * 1000}
+  );
+  const parsedData = ChannelsPaginated.parse(JSON.parse(cliOutput.toString()));
+  return parsedData.channels
 }
