58f5036780f3ae4eff8f7c12094ea5441ba311bd
inkvi <374203+Inkvi@users.noreply.github.com>
2024-01-08 22:20:05 -0600

    Show all packets with sent states

--- app/api/ibc/[type]/packets.ts
+++ app/api/ibc/[type]/packets.ts
@@ -9,30 +9,60 @@ import Abi from "../../../contracts/Dispatcher.json";
 
 export const dynamic = 'force-dynamic' // defaults to auto
 
+export interface PacketData {
+  sequence: string;
+  sourcePortAddress: string;
+  sourceChannelId: string;
+  packet: string;
+  timeout: string;
+  fee: string;
+  id: string;
+  state: string;
+}
+
 export async function getPackets(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const from = searchParams.get('from')
   const to = searchParams.get('to')
-  const chain = searchParams.get('chain')
+  const chainFrom = searchParams.get('chainFrom')
+  const chainTo = searchParams.get('chainTo')
   const dispatcher = searchParams.get('dispatcher')
 
-  if (!from || !to || !chain) {
+  if (!from || !chainFrom || !chainTo) {
     return NextResponse.error()
   }
 
   const fromBlock = Number(from)
-  const toBlock = Number(to)
-  const chainId = chain as CHAIN
-  const dispatcherAddress = dispatcher ?? CHAIN_CONFIGS[chainId].dispatcher;
+  const toBlock = to ? Number(to) : "latest"
+  const chainFromId = chainFrom as CHAIN
+  const chainToId = chainTo as CHAIN
+  const dispatcherFromAddress = dispatcher ?? CHAIN_CONFIGS[chainFromId].dispatcher;
 
-  const provider = new CachingJsonRpcProvider(CHAIN_CONFIGS[chainId].rpc, CHAIN_CONFIGS[chainId].id);
-  const contract = new ethers.Contract(dispatcherAddress, Abi.abi, provider);
+  const providerFrom = new CachingJsonRpcProvider(CHAIN_CONFIGS[chainFromId].rpc, CHAIN_CONFIGS[chainFromId].id);
+  const contractFrom = new ethers.Contract(dispatcherFromAddress, Abi.abi, providerFrom);
 
-  const ackLogs = (await contract.queryFilter('Acknowledgement', fromBlock, toBlock)) as Array<ethers.EventLog>;
-  const sendPacketLogs = (await contract.queryFilter('SendPacket', fromBlock, toBlock)) as Array<ethers.EventLog>;
-  const recvPacketLogs = (await contract.queryFilter('RecvPacket', fromBlock, toBlock)) as Array<ethers.EventLog>;
+  const ackLogs = (await contractFrom.queryFilter('Acknowledgement', fromBlock, toBlock)) as Array<ethers.EventLog>;
+  const sendPacketLogs = (await contractFrom.queryFilter('SendPacket', fromBlock, toBlock)) as Array<ethers.EventLog>;
+  const recvPacketLogs = (await contractFrom.queryFilter('RecvPacket', fromBlock, toBlock)) as Array<ethers.EventLog>;
 
   console.log("ackLogs: ", ackLogs, "sendPacketLogs: ", sendPacketLogs, "recvPacketLogs: ", recvPacketLogs)
 
-  return Response.json({dispatcherAddress})
+  const response: PacketData[] = []
+  sendPacketLogs.forEach((sendPacketLog) => {
+    const [sourcePortAddress, sourceChannelId, packet, sequence, timeout, fee] = sendPacketLog.args;
+    const key = `${sourcePortAddress}-${sourceChannelId}-${sequence}`;
+    response.push({
+      sourcePortAddress,
+      sourceChannelId,
+      packet,
+      fee,
+      sequence: sequence.toString(),
+      timeout: timeout.toString(),
+      id: key,
+      state: "SENT"
+    })
+  });
+
+
+  return Response.json(response)
 }
