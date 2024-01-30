53d31d2abb0b36ca05b8e5b1af1643f35b9811fb
Alexander <alex@inkvi.com>
2024-01-30 11:10:07 -0800

    Configure dispatcher addresses per chain using env var

--- app/chains.ts
+++ app/chains.ts
@@ -3,6 +3,8 @@ import { Chain } from "./types/chain";
 import { OptimismIcon, BaseIcon } from "./icons/Chains";
 
 export type CHAIN = 'optimism' | 'base';
+let opDispatcher = process.env.OP_DISPATCHER!;
+let baseDispatcher = process.env.BASE_DISPATCHER!;
 
 export const CHAIN_CONFIGS: {
   [key in CHAIN]: Chain;
@@ -11,7 +13,7 @@ export const CHAIN_CONFIGS: {
     id: 11155420,
     display: "Optimism",
     rpc: "https://opt-sepolia.g.alchemy.com/v2/RN7slh_2cUIuzhxo4M9VgYCbqRcPOmkJ",
-    dispatcher: "0xD92B86315CBcf9cC612F0b0542E0bE5871bCa146",
+    dispatcher: opDispatcher,
     blockTime: 2,
     icon: OptimismIcon,
   },
@@ -19,7 +21,7 @@ export const CHAIN_CONFIGS: {
     id: 84532,
     display: "Base",
     rpc: "https://base-sepolia.g.alchemy.com/v2/zGVxj0T-xvSR29_t7MlIhqRskkSwugVM",
-    dispatcher: "0xab6AEF0311954C40AcD4D1DED56CAAE9cc074975",
+    dispatcher: baseDispatcher,
     blockTime: 2,
     icon: BaseIcon,
   }
