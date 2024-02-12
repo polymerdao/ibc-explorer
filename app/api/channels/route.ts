1884e817221612828b99a9aa383ac5d980bb467b
mvpoyatt <mpoyatt@gmail.com>
2024-02-12 16:20:43 -0500

    Disable caching in channels route

--- app/api/channels/route.ts
+++ app/api/channels/route.ts
@@ -1,6 +1,8 @@
 import { QueryChannelsResponse } from "cosmjs-types/ibc/core/channel/v1/query";
 import { getTmClient } from "@/api/utils/cosmos";
 
+export const dynamic = 'force-dynamic' // defaults to auto
+
 export async function GET() {
   const tmClient = await getTmClient(process.env.API_URL!)
   const channels = await tmClient.ibc.channel.allChannels()
