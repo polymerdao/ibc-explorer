3cd7ecb057d137f9046537368e13ec0392c28edf
mvpoyatt <mpoyatt@gmail.com>
2024-02-16 17:03:45 -0500

    Basic table filtering by column (text input and dropdowns), stashing progress

--- app/api/mock-packets/route.ts
+++ app/api/mock-packets/route.ts
@@ -1,29 +1,43 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Packet, PacketStates } from "utils/types/packet";
 
-export function GET() {
-  let packets: Packet[] = [];
-  for (let i = 0; i < 10; i++) {
-    packets.push(testPacket);
+function getRandomHexString(length: number): string {
+  const characters = 'abcdef0123456789';
+  let result = '0x';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+export function GET(request: NextRequest) {
+  const reqParams = request.nextUrl.searchParams;
+  const size = reqParams.get("size") ? parseInt(reqParams.get("size") as string) : 10;
+  const packets: Packet[] = [];
+
+  for (let i = 1; i <= size; i++) {
+    const createTime = Date.now();
+    const endTime = createTime + Math.floor(Math.random() * (120000 - 7000)) + 7000; // Random time between 7 and 120 seconds
+
+    const packet: Packet = {
+      sequence: i.toString(),
+      sourcePortAddress: getRandomHexString(64),
+      sourceChannelId: `channel-${i}`,
+      destPortAddress: getRandomHexString(40),
+      destChannelId: `channel-${i + 1}`,
+      timeout: "1000",
+      fee: "1ETH",
+      id: `packet-${i}`,
+      state: PacketStates.ACK,
+      createTime,
+      endTime,
+      sendTx: getRandomHexString(64),
+      rcvTx: getRandomHexString(64),
+      ackTx: getRandomHexString(64),
+      sourceChain: Math.random() < 0.5 ? "optimism" : "base",
+      destChain: Math.random() < 0.5 ? "optimism" : "base",
+    };
+    packets.push(packet);
   }
   return NextResponse.json(packets);
 }
-
-const testPacket: Packet = {
-  sequence: "3",
-  sourcePortAddress: "0xedd5599e42e5f8ce2a6442348337ac344c6e11ed6436d446fd8025f27e6a67eb",
-  sourceChannelId: "channel-11",
-  destPortAddress: "0x03b308b0091c8d1fec5406cff741352a2e66ae28",
-  destChannelId: "channel-12",
-  timeout: "1000",
-  fee: "1ETH",
-  id: "packet-1",
-  state: PacketStates.ACK,
-  createTime: 1630000000,
-  endTime: 1630010000,
-  sendTx: "0xedd5599e42e5f8ce2a6442348337ac344c6e11ed6436d446fd8025f27e6a67eb",
-  rcvTx: "0xa58af813c09e47b3e8288e92c72c5a11ecf7249492e95ba17bc888a7ffa7786d",
-  ackTx: "0x8b965a68268506322a428252f18274ba7c20fb6a87d9d36d949aa94e4b32700b",
-  sourceChain: "optimism",
-  destChain: "base"
-};
