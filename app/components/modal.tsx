5b2980543a932cb7a9a7105ca2e4a4a138ea0ea4
mvpoyatt <mpoyatt@gmail.com>
2024-02-22 18:00:21 -0500

    Made reusable modal component and handled error response from fetch request

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -1,59 +1,53 @@
-import { Dialog, Transition } from '@headlessui/react';
 import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { FiX } from 'react-icons/fi';
 
-export function Modal({open, setOpen}: {open: boolean, setOpen: (open: boolean) => void}) {
+export function Modal({open, setOpen, title, content}: {open: boolean, setOpen: (open: boolean) => void, title: string, content: JSX.Element}) {
   return (
 
     <Transition appear show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={() => setOpen(false)}>
+      <Dialog className="relative z-10" onClose={() => setOpen(false)}>
+
         <Transition.Child
           as={Fragment}
-          enter="ease-out duration-300"
+          enter="ease-out duration-200"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="ease-in duration-200"
+          leave="ease-in duration-150"
           leaveFrom="opacity-100"
           leaveTo="opacity-0">
-          <div className="fixed inset-0 bg-black/25" />
+          <div className="fixed inset-0 bg-bg-dark/40 dark:bg-bg-dark/60 backdrop-blur-sm" />
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div className="flex min-h-full items-center justify-center p-4">
+
             <Transition.Child
               as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95" >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                <Dialog.Title
-                  as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900">
-                  Payment successful
+              enter="ease-out duration-200"
+              enterFrom="opacity-0 translate-y-6"
+              enterTo="opacity-100 translate-y-0"
+              leave="ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-6">
+              <Dialog.Panel
+                className="w-fit max-w-full min-w-36 transform border border-slate-800 dark:border-slate-300 rounded-md bg-bg-light dark:bg-bg-dark p-6 transition-all">
+                <button className="absolute top-0 right-0 m-4"
+                  onClick={() => setOpen(false)}>
+                  <FiX className="w-6 h-6" />
+                </button>
+                <Dialog.Title className="text-lg font-medium pr-10">
+                  {title}
                 </Dialog.Title>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    Your payment has been successfully submitted. We’ve sent
-                    you an email with all of the details of your order.
-                  </p>
-                </div>
-
-                <div className="mt-4">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={() => setOpen(false)}>
-                    Got it, thanks!
-                  </button>
+                <div className="mt-4 mb-2 pr-4 text-wrap">
+                  {content}
                 </div>
               </Dialog.Panel>
             </Transition.Child>
+
           </div>
         </div>
       </Dialog>
     </Transition>
-
   );
 }
