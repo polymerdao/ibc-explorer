41e444b447fb5064151ab86e7653497956dd86b7
Derek <derek@polymerlabs.org>
2024-02-29 11:17:28 -0800

    add support for second dispatcher address for simclient

--- app/utils/chains/configs.ts
+++ app/utils/chains/configs.ts
@@ -5,6 +5,9 @@ export type CHAIN = 'optimism' | 'base';
 
 let opDispatcher = process.env.DISPATCHER_ADDRESS_OPTIMISM!;
 let baseDispatcher = process.env.DISPATCHER_ADDRESS_BASE!;
+let opDispatcherSimClient = process.env.DISPATCHER_ADDRESS_OPTIMISM_SIMCLIENT!;
+let baseDispatcherSimClient = process.env.DISPATCHER_ADDRESS_BASE_SIMCLIENT!;
+
 
 export const CHAIN_CONFIGS: {
   [key in CHAIN]: Chain;
@@ -13,7 +16,7 @@ export const CHAIN_CONFIGS: {
     id: 11155420,
     display: "Optimism",
     rpc: "https://opt-sepolia.g.alchemy.com/v2/RN7slh_2cUIuzhxo4M9VgYCbqRcPOmkJ",
-    dispatcher: opDispatcher,
+    dispatchers: [opDispatcher, opDispatcherSimClient],
     blockTime: 2,
     icon: OptimismIcon,
   },
@@ -21,7 +24,7 @@ export const CHAIN_CONFIGS: {
     id: 84532,
     display: "Base",
     rpc: "https://base-sepolia.g.alchemy.com/v2/zGVxj0T-xvSR29_t7MlIhqRskkSwugVM",
-    dispatcher: baseDispatcher,
+    dispatchers: [baseDispatcher, baseDispatcherSimClient],
     blockTime: 2,
     icon: BaseIcon,
   }
