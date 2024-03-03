c82727949e0c57c9d12ded4f4697d6b20de78059
Michael Poyatt <poyattm@gmail.com>
2024-03-03 07:34:26 -0500

    Revert "add support for second dispatcher address for simclient"
    This reverts commit 102ed4be6d23a7e207d3837bf02582590c16a23c.

--- app/utils/chains/configs.ts
+++ app/utils/chains/configs.ts
@@ -5,9 +5,6 @@ export type CHAIN = 'optimism' | 'base';
 
 let opDispatcher = process.env.DISPATCHER_ADDRESS_OPTIMISM!;
 let baseDispatcher = process.env.DISPATCHER_ADDRESS_BASE!;
-let opDispatcherSimClient = process.env.DISPATCHER_ADDRESS_OPTIMISM_SIMCLIENT!;
-let baseDispatcherSimClient = process.env.DISPATCHER_ADDRESS_BASE_SIMCLIENT!;
-
 
 export const CHAIN_CONFIGS: {
   [key in CHAIN]: Chain;
@@ -16,7 +13,7 @@ export const CHAIN_CONFIGS: {
     id: 11155420,
     display: "Optimism",
     rpc: "https://opt-sepolia.g.alchemy.com/v2/RN7slh_2cUIuzhxo4M9VgYCbqRcPOmkJ",
-    dispatchers: [opDispatcher, opDispatcherSimClient],
+    dispatcher: opDispatcher,
     blockTime: 2,
     icon: OptimismIcon,
   },
@@ -24,7 +21,7 @@ export const CHAIN_CONFIGS: {
     id: 84532,
     display: "Base",
     rpc: "https://base-sepolia.g.alchemy.com/v2/zGVxj0T-xvSR29_t7MlIhqRskkSwugVM",
-    dispatchers: [baseDispatcher, baseDispatcherSimClient],
+    dispatcher: baseDispatcher,
     blockTime: 2,
     icon: BaseIcon,
   }
