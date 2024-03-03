fde2121929389983a7f53a82b379873711a1ff37
Michael Poyatt <poyattm@gmail.com>
2024-03-03 07:34:26 -0500

    Revert "Initial connnections page commit, added table"
    This reverts commit 7aecc6f398ce7c1c43f3080d3610bfea225a9947.

--- app/api/channels/route.ts
+++ app/api/channels/route.ts
@@ -1,6 +1,6 @@
-import { GetTmClient } from "api/utils/cosmos";
 import { QueryChannelsResponse } from "cosmjs-types/ibc/core/channel/v1/query";
 import { NextResponse } from "next/server";
+import { GetTmClient } from "api/utils/cosmos";
 
 export const dynamic = 'force-dynamic' // defaults to auto
 export type { IdentifiedChannel } from "cosmjs-types/ibc/core/channel/v1/channel";
