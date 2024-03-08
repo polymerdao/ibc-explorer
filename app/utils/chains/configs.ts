23950644a1d30a82df12b71d0477b6b44310944b
Derek <derek@polymerlabs.org>
2024-03-07 22:23:59 -0800

    add support for molten chain

--- app/utils/chains/configs.ts
+++ app/utils/chains/configs.ts
@@ -1,25 +1,31 @@
 import { Chain } from 'utils/types/chain';
-import { OptimismIcon, BaseIcon } from './icons';
+import { OptimismIcon, BaseIcon, MoltenIcon } from './icons';
 
-export type CHAIN = 'optimism' | 'base';
+export type CHAIN = 'optimism' | 'base' | 'molten';
 
 let opDispatcher = process.env.DISPATCHER_ADDRESS_OPTIMISM!;
-let baseDispatcher = process.env.DISPATCHER_ADDRESS_BASE!;
 let opDispatcherSimClient = process.env.DISPATCHER_ADDRESS_OPTIMISM_SIMCLIENT!;
+let baseDispatcher = process.env.DISPATCHER_ADDRESS_BASE!;
 let baseDispatcherSimClient = process.env.DISPATCHER_ADDRESS_BASE_SIMCLIENT!;
-
+let moltenDispatcher = process.env.DISPATCHER_ADDRESS_MOLTEN!;
+let moltenDispatcherSimClient =
+  process.env.DISPATCHER_ADDRESS_MOLTEN_SIMCLIENT!;
 
 let opClientName = process.env.OPTIMISM_CLIENT_NAME!;
 let baseClientName = process.env.BASE_CLIENT_NAME!;
+let moltenClientName = process.env.MOLTEN_CLIENT_NAME!;
 let opClientSimClientName = process.env.OPTIMISM_CLIENT_SIMCLIENT_NAME!;
 let baseClientSimClientName = process.env.BASE_CLIENT_SIMCLIENT_NAME!;
+let moltenClientSimClientName = process.env.MOLTEN_CLIENT_SIMCLIENT_NAME!;
 
 let optimismRPC =
   process.env.OPTIMISM_RPC ||
-  'https://opt-sepolia.g.alchemy.com/v2/iMhzwCPw18v9Byeh59cedtUKbul_jFF3'; // "https://opt-sepolia.g.alchemy.com/v2/jKvLhhXvtnWdNeZrKst0demxnwJcYH1o"
+  'https://opt-sepolia.g.alchemy.com/v2/jKvLhhXvtnWdNeZrKst0demxnwJcYH1o';
 let baseRPC =
   process.env.BASE_RPC ||
-  'https://base-sepolia.g.alchemy.com/v2/ivsMbH2_FVzYzYrDY5-eaWenWl2Bo_QK'; // "https://base-sepolia.g.alchemy.com/v2/776dC6qT-NTtupdnxlUJuXGbUIKWWhLe"
+  'https://base-sepolia.g.alchemy.com/v2/776dC6qT-NTtupdnxlUJuXGbUIKWWhLe';
+let moltenRPC =
+  process.env.MOLTEN_RPC || 'https://unidex-sepolia.rpc.caldera.xyz/http';
 
 export const CHAIN_CONFIGS: {
   [key in CHAIN]: Chain;
@@ -41,5 +47,14 @@ export const CHAIN_CONFIGS: {
     clients: [baseClientName, baseClientSimClientName],
     blockTime: 2,
     icon: BaseIcon,
+  },
+  molten: {
+    id: 49483,
+    display: 'Molten',
+    rpc: moltenRPC,
+    dispatchers: [moltenDispatcher, moltenDispatcherSimClient],
+    clients: [moltenClientName, moltenClientSimClientName],
+    blockTime: 2,
+    icon: MoltenIcon,
   },
 };
--- app/utils/chains/icons.tsx
+++ app/utils/chains/icons.tsx
@@ -18,3 +18,14 @@ export const BaseIcon = (): JSX.Element => {
     </svg>
   );
 };
+
+// TODO: fix the molten icon
+export const MoltenIcon = (): JSX.Element => {
+  return (
+    <svg className="mr-1.5" width="30" height="30" viewBox="0 0 111 111" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M54.921 110.034C85.359 110.034 110.034 85.402 110.034 55.017C110.034 24.6319 85.359 0 54.921 0C26.0432 0 2.35281 22.1714 0 50.3923H72.8467V59.6416H3.9565e-07C2.35281 87.8625 26.0432 110.034 54.921 110.034Z"
+        fill="#0052FF"/>
+    </svg>
+  );
+};
