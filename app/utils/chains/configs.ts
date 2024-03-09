3c6fbd09eccb651662d815993516ce127b060abd
Derek <derek@polymerlabs.org>
2024-03-09 13:07:03 -0800

    update chain rpcs

--- app/utils/chains/configs.ts
+++ app/utils/chains/configs.ts
@@ -1,5 +1,5 @@
-import { Chain } from "utils/types/chain";
-import { OptimismIcon, BaseIcon } from "./icons";
+import { Chain } from 'utils/types/chain';
+import { OptimismIcon, BaseIcon } from './icons';
 
 export type CHAIN = 'optimism' | 'base';
 
@@ -8,23 +8,30 @@ let baseDispatcher = process.env.DISPATCHER_ADDRESS_BASE!;
 let opDispatcherSimClient = process.env.DISPATCHER_ADDRESS_OPTIMISM_SIMCLIENT!;
 let baseDispatcherSimClient = process.env.DISPATCHER_ADDRESS_BASE_SIMCLIENT!;
 
+let optimismRPC =
+  process.env.OPTIMISM_RPC ||
+  'https://opt-sepolia.g.alchemy.com/v2/iMhzwCPw18v9Byeh59cedtUKbul_jFF3'; // "https://opt-sepolia.g.alchemy.com/v2/jKvLhhXvtnWdNeZrKst0demxnwJcYH1o"
+let baseRPC =
+  process.env.BASE_RPC ||
+  'https://base-sepolia.g.alchemy.com/v2/ivsMbH2_FVzYzYrDY5-eaWenWl2Bo_QK'; // "https://base-sepolia.g.alchemy.com/v2/776dC6qT-NTtupdnxlUJuXGbUIKWWhLe"
+
 export const CHAIN_CONFIGS: {
   [key in CHAIN]: Chain;
 } = {
   optimism: {
     id: 11155420,
-    display: "Optimism",
-    rpc: "https://opt-sepolia.g.alchemy.com/v2/jKvLhhXvtnWdNeZrKst0demxnwJcYH1o",
+    display: 'Optimism',
+    rpc: optimismRPC,
     dispatchers: [opDispatcher, opDispatcherSimClient],
     blockTime: 2,
     icon: OptimismIcon,
   },
   base: {
     id: 84532,
-    display: "Base",
-    rpc: "https://base-sepolia.g.alchemy.com/v2/776dC6qT-NTtupdnxlUJuXGbUIKWWhLe",
+    display: 'Base',
+    rpc: baseRPC,
     dispatchers: [baseDispatcher, baseDispatcherSimClient],
     blockTime: 2,
     icon: BaseIcon,
-  }
+  },
 };
