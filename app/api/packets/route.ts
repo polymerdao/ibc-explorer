6aad336dd6bda4c815347b6a48b64cf6f4a7935c
Derek <derek@polymerlabs.org>
2024-03-09 14:31:55 -0800

    limit block range to last 100k blocks

--- app/api/packets/route.ts
+++ app/api/packets/route.ts
@@ -8,27 +8,22 @@ import { IdentifiedChannel } from 'cosmjs-types/ibc/core/channel/v1/channel';
 import { QueryChannelsResponse } from 'cosmjs-types/ibc/core/channel/v1/query';
 import Abi from 'utils/dispatcher.json';
 
-export const dynamic = 'force-dynamic' // defaults to auto
+export const dynamic = 'force-dynamic'; // defaults to auto
 
 async function getChannels() {
   const tmClient = await GetTmClient();
   const channels = await tmClient.ibc.channel.allChannels();
-  return (QueryChannelsResponse.toJSON(channels) as QueryChannelsResponse).channels;
+  return (QueryChannelsResponse.toJSON(channels) as QueryChannelsResponse)
+    .channels;
 }
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const from = searchParams.get('from');
-  const to = searchParams.get('to')
+  const to = searchParams.get('to');
 
   if (!from) {
     return NextResponse.error();
-  }
-
-  const cache = SimpleCache.getInstance()
-  const allPackets = cache.get("allPackets");
-  if (allPackets) {
-    return NextResponse.json(allPackets);
   }
 
   const channelsResponse = await getChannels();
@@ -41,7 +36,7 @@ export async function GET(request: NextRequest) {
       channel.portId.startsWith(`polyibc.`) &&
       channel.counterparty.portId.startsWith(`polyibc.`)
     );
-  })
+  });
   if (openChannels.length === 0) {
     return NextResponse.json([]);
   }
@@ -49,25 +44,49 @@ export async function GET(request: NextRequest) {
   const validChannelIds = new Set<string>();
   openChannels.forEach((channel) => {
     validChannelIds.add(channel.channelId);
-  })
+  });
 
   const fromBlock = Number(from);
-  const toBlock = to ? Number(to) : "latest";
+  const toBlock = to ? Number(to) : 'latest';
 
   // Collect send logs from all chains
   let sendLogs: Array<[ethers.EventLog, CHAIN]> = [];
-  let srcChainProviders: Record<CHAIN, CachingJsonRpcProvider> = {} as Record<CHAIN, CachingJsonRpcProvider>;
+  let srcChainProviders: Record<CHAIN, CachingJsonRpcProvider> = {} as Record<
+    CHAIN,
+    CachingJsonRpcProvider
+  >;
   let srcChainContracts: Array<[ethers.Contract, CHAIN]> = [];
   for (const chain in CHAIN_CONFIGS) {
     const chainId = chain as CHAIN;
     const dispatcherAddresses = CHAIN_CONFIGS[chainId].dispatchers;
+    const provider = new CachingJsonRpcProvider(
+      CHAIN_CONFIGS[chainId].rpc,
+      CHAIN_CONFIGS[chainId].id
+    );
+    srcChainProviders[chainId] = provider;
+    let latestBlock = await provider.getBlockNumber();
+
     for (const dispatcherAddress of dispatcherAddresses) {
-      const provider = new CachingJsonRpcProvider(CHAIN_CONFIGS[chainId].rpc, CHAIN_CONFIGS[chainId].id);
-      srcChainProviders[chainId] = provider;
-      const contract = new ethers.Contract(dispatcherAddress, Abi.abi, provider);
+      const contract = new ethers.Contract(
+        dispatcherAddress,
+        Abi.abi,
+        provider
+      );
+
       srcChainContracts.push([contract, chainId]);
-      const newSendLogs = (await contract.queryFilter('SendPacket', fromBlock, toBlock)) as Array<ethers.EventLog>;
-      sendLogs = sendLogs.concat(newSendLogs.map((eventLog) => [eventLog, chainId]));
+
+      // query the last 100000 blocks ~2.5 days
+      let fromBlock = latestBlock - 100000;
+      let toBlock = latestBlock;
+
+      const newSendLogs = (await contract.queryFilter(
+        'SendPacket',
+        fromBlock,
+        toBlock
+      )) as Array<ethers.EventLog>;
+      sendLogs = sendLogs.concat(
+        newSendLogs.map((eventLog) => [eventLog, chainId])
+      );
     }
   }
 
@@ -77,21 +96,27 @@ export async function GET(request: NextRequest) {
 
   async function processSendLog(sendLog: [ethers.EventLog, CHAIN]) {
     const [sendEvent, srcChain] = sendLog;
-    let [srcPortAddress, srcChannelId, packet, sequence, timeout, fee] = sendEvent.args;
+    let [srcPortAddress, srcChannelId, packet, sequence, timeout, fee] =
+      sendEvent.args;
     srcChannelId = ethers.decodeBytes32String(srcChannelId);
 
     if (!validChannelIds.has(srcChannelId)) {
-      console.log("Skipping packet for channel: ", srcChannelId);
+      console.log('Skipping packet for channel: ', srcChannelId);
       return;
     }
 
-    const channel = openChannels.find(channel => (
-      channel.channelId === srcChannelId &&
-      channel.portId.startsWith(`polyibc.${srcChain}`) &&
-      channel.portId.endsWith(srcPortAddress.slice(2))
-    ));
+    const channel = openChannels.find(
+      (channel) =>
+        channel.channelId === srcChannelId &&
+        channel.portId.startsWith(`polyibc.${srcChain}`) &&
+        channel.portId.endsWith(srcPortAddress.slice(2))
+    );
     if (!channel) {
-      console.warn("No channel found for packet: ", srcChannelId, srcPortAddress);
+      console.warn(
+        'No channel found for packet: ',
+        srcChannelId,
+        srcPortAddress
+      );
       return;
     }
 
@@ -111,29 +136,35 @@ export async function GET(request: NextRequest) {
       state: PacketStates.SENT,
       createTime: srcBlock!.timestamp,
       sendTx: sendEvent.transactionHash,
-      sourceChain: channel.portId.split(".")[1],
-      destChain: channel.counterparty.portId.split(".")[1],
+      sourceChain: channel.portId.split('.')[1],
+      destChain: channel.counterparty.portId.split('.')[1],
     };
     unprocessedPacketKeys.add(key);
   }
-  await Promise.allSettled(sendLogs.map(processSendLog))
+  await Promise.allSettled(sendLogs.map(processSendLog));
 
   /*
-  ** Find the state of each packet
-  ** States could be like:
-  ** SENT (event on L2), POLY_RECV (received by Polymer), POLY_SENT (committed on Polymer), RECV (event on L2),
-  ** WRITE_ACK (event on L2), POLY_ACK_RECV (ack received on Polymer), POLY_WRITE_ACK (ack written on Polymer), ACK (event on L2)
-  **
-  ** To set a proper state for each packet, start with SENT state for all relevant packets, then:
-  ** For each packet go into the reverse direction of the packet flow starting from ACK
-  ** If a packet reached the corresponding state, set it as the state for the packet and move on to the next packet
-  ** Otherwise move to the next state until SENT state is reached
-  */
+   ** Find the state of each packet
+   ** States could be like:
+   ** SENT (event on L2), POLY_RECV (received by Polymer), POLY_SENT (committed on Polymer), RECV (event on L2),
+   ** WRITE_ACK (event on L2), POLY_ACK_RECV (ack received on Polymer), POLY_WRITE_ACK (ack written on Polymer), ACK (event on L2)
+   **
+   ** To set a proper state for each packet, start with SENT state for all relevant packets, then:
+   ** For each packet go into the reverse direction of the packet flow starting from ACK
+   ** If a packet reached the corresponding state, set it as the state for the packet and move on to the next packet
+   ** Otherwise move to the next state until SENT state is reached
+   */
 
   // Start by searching for ack events on the source chains
   const ackLogsPromises = srcChainContracts.map(async ([contract, chain]) => {
-    const newAckLogs = (await contract.queryFilter('Acknowledgement', fromBlock, toBlock)) as Array<ethers.EventLog>;
-    return newAckLogs.map((eventLog) => [eventLog, chain] as [ethers.EventLog, CHAIN]);
+    const newAckLogs = (await contract.queryFilter(
+      'Acknowledgement',
+      fromBlock,
+      toBlock
+    )) as Array<ethers.EventLog>;
+    return newAckLogs.map(
+      (eventLog) => [eventLog, chain] as [ethers.EventLog, CHAIN]
+    );
   });
 
   const ackLogsResults = await Promise.allSettled(ackLogsPromises);
@@ -145,14 +176,15 @@ export async function GET(request: NextRequest) {
     }
   }
 
-
   async function processAckLog(ackLog: [ethers.EventLog, CHAIN]) {
     const [ackEvent, srcChain] = ackLog;
     let [srcPortAddress, srcChannelId, sequence] = ackEvent.args;
-    const key = `${srcPortAddress}-${ethers.decodeBytes32String(srcChannelId)}-${sequence}`;
+    const key = `${srcPortAddress}-${ethers.decodeBytes32String(
+      srcChannelId
+    )}-${sequence}`;
 
     if (!packets[key]) {
-      console.log("No packet found for ack: ", key);
+      console.log('No packet found for ack: ', key);
       return;
     }
 
@@ -172,15 +204,25 @@ export async function GET(request: NextRequest) {
   await Promise.allSettled(ackLogs.map(processAckLog));
 
   // Set up providers and contracts to interact with the destination chains
-  let destChainProviders: Record<CHAIN, CachingJsonRpcProvider> = {} as Record<CHAIN, CachingJsonRpcProvider>;
+  let destChainProviders: Record<CHAIN, CachingJsonRpcProvider> = {} as Record<
+    CHAIN,
+    CachingJsonRpcProvider
+  >;
   let destChainContracts: Array<[ethers.Contract, CHAIN]> = [];
   for (const chain in CHAIN_CONFIGS) {
     const chainId = chain as CHAIN;
     const dispatcherAddresses = CHAIN_CONFIGS[chainId].dispatchers;
     for (const dispatcherAddress of dispatcherAddresses) {
-      const provider = new CachingJsonRpcProvider(CHAIN_CONFIGS[chainId].rpc, CHAIN_CONFIGS[chainId].id);
+      const provider = new CachingJsonRpcProvider(
+        CHAIN_CONFIGS[chainId].rpc,
+        CHAIN_CONFIGS[chainId].id
+      );
       destChainProviders[chainId] = provider;
-      const contract = new ethers.Contract(dispatcherAddress, Abi.abi, provider);
+      const contract = new ethers.Contract(
+        dispatcherAddress,
+        Abi.abi,
+        provider
+      );
       destChainContracts.push([contract, chainId]);
     }
   }
@@ -192,23 +234,36 @@ export async function GET(request: NextRequest) {
     const packet = packets[key];
 
     try {
-      const ack = await tmClient.ibc.channel.packetAcknowledgement(packet.destPortAddress, packet.destChannelId, Number(packet.sequence));
+      const ack = await tmClient.ibc.channel.packetAcknowledgement(
+        packet.destPortAddress,
+        packet.destChannelId,
+        Number(packet.sequence)
+      );
       if (ack.acknowledgement) {
         packet.state = PacketStates.POLY_WRITE_ACK;
         unprocessedPacketKeys.delete(key);
       }
-    } catch (e) {
-    }
+    } catch (e) {}
   };
 
-  await Promise.allSettled(Array.from(unprocessedPacketKeys).map(processPacket));
+  await Promise.allSettled(
+    Array.from(unprocessedPacketKeys).map(processPacket)
+  );
 
   // It seems that due to short circuiting POLY_ACK_RECV can't be distinguished as a separate state so this state is skipped
 
-  const writeAckLogsPromises = destChainContracts.map(async ([destContract, destChain]) => {
-    const newWriteAckLogs = await destContract.queryFilter('WriteAckPacket', 1, "latest");
-    return newWriteAckLogs.map((eventLog) => [eventLog, destChain] as [ethers.EventLog, CHAIN]);
-  });
+  const writeAckLogsPromises = destChainContracts.map(
+    async ([destContract, destChain]) => {
+      const newWriteAckLogs = await destContract.queryFilter(
+        'WriteAckPacket',
+        1,
+        'latest'
+      );
+      return newWriteAckLogs.map(
+        (eventLog) => [eventLog, destChain] as [ethers.EventLog, CHAIN]
+      );
+    }
+  );
 
   let writeAckLogs: Array<[ethers.EventLog, CHAIN]> = [];
   for (const result of await Promise.allSettled(writeAckLogsPromises)) {
@@ -223,14 +278,20 @@ export async function GET(request: NextRequest) {
 
     const channel = openChannels.find((channel) => {
       return (
-        channel.counterparty.channelId === ethers.decodeBytes32String(destChannelId) && 
+        channel.counterparty.channelId ===
+          ethers.decodeBytes32String(destChannelId) &&
         channel.counterparty.portId.startsWith(`polyibc.${destChain}`) &&
         channel.counterparty.portId.endsWith(receiver.slice(2))
       );
-    })
+    });
 
     if (!channel) {
-      console.log("Unable to find channel for write ack: ", destChannelId, "receiver: ", receiver);
+      console.log(
+        'Unable to find channel for write ack: ',
+        destChannelId,
+        'receiver: ',
+        receiver
+      );
       continue;
     }
 
@@ -242,11 +303,19 @@ export async function GET(request: NextRequest) {
   }
 
   // Match any recv packet events on destination chains to packets
-  const recvPacketPromises = destChainContracts.map(async (destChainContract) => {
-    const [destContract, destChain] = destChainContract;
-    const newRecvPacketLogs = await destContract.queryFilter('RecvPacket', 1, "latest");
-    return newRecvPacketLogs.map((eventLog) => [eventLog, destChain] as [ethers.EventLog, CHAIN]);
-  });
+  const recvPacketPromises = destChainContracts.map(
+    async (destChainContract) => {
+      const [destContract, destChain] = destChainContract;
+      const newRecvPacketLogs = await destContract.queryFilter(
+        'RecvPacket',
+        1,
+        'latest'
+      );
+      return newRecvPacketLogs.map(
+        (eventLog) => [eventLog, destChain] as [ethers.EventLog, CHAIN]
+      );
+    }
+  );
 
   let recvPacketLogs: Array<[ethers.EventLog, CHAIN]> = [];
   for (const result of await Promise.allSettled(recvPacketPromises)) {
@@ -261,20 +330,30 @@ export async function GET(request: NextRequest) {
 
     const channel = openChannels.find((channel) => {
       return (
-        channel.counterparty.channelId === ethers.decodeBytes32String(destChannelId) &&
+        channel.counterparty.channelId ===
+          ethers.decodeBytes32String(destChannelId) &&
         channel.counterparty.portId.startsWith(`polyibc.${destChain}`) &&
         channel.counterparty.portId.endsWith(destPortAddress.slice(2))
       );
     });
 
     if (!channel) {
-      console.log("Unable to find channel for recv packet: ", destChannelId, "receiver: ", destPortAddress);
+      console.log(
+        'Unable to find channel for recv packet: ',
+        destChannelId,
+        'receiver: ',
+        destPortAddress
+      );
       return;
     }
 
-    const key = `0x${channel.portId.split(".")[2]}-${channel.channelId}-${sequence}`;
+    const key = `0x${channel.portId.split('.')[2]}-${
+      channel.channelId
+    }-${sequence}`;
     if (packets[key]) {
-      const recvBlock = await destChainProviders[destChain].getBlock(recvPacketEvent.blockNumber);
+      const recvBlock = await destChainProviders[destChain].getBlock(
+        recvPacketEvent.blockNumber
+      );
 
       if (recvBlock!.timestamp < packets[key].createTime) {
         return;
@@ -297,39 +376,45 @@ export async function GET(request: NextRequest) {
       const packet = packets[key];
 
       try {
-        const packetCommitment = await tmClient.ibc.channel.packetCommitment(packet.sourcePortAddress, packet.sourceChannelId, Number(packet.sequence));
+        const packetCommitment = await tmClient.ibc.channel.packetCommitment(
+          packet.sourcePortAddress,
+          packet.sourceChannelId,
+          Number(packet.sequence)
+        );
         if (packetCommitment.commitment) {
           packet.state = PacketStates.POLY_WRITE_ACK;
           unprocessedPacketKeys.delete(key);
         }
-      } catch (e) {
-      }
+      } catch (e) {}
     })
   );
 
   // Match any recv packet events on Polymer to packets
-  await Promise.allSettled(Array.from(unprocessedPacketKeys).map(async key => {
-    const packet = packets[key]; // Assuming `packets` is defined and contains the details for each key
+  await Promise.allSettled(
+    Array.from(unprocessedPacketKeys).map(async (key) => {
+      const packet = packets[key]; // Assuming `packets` is defined and contains the details for each key
 
-    try {
-      const packetReceipt = await tmClient.ibc.channel.packetReceipt(
-        packet.destPortAddress, packet.destChannelId, Number(packet.sequence)
-      );
+      try {
+        const packetReceipt = await tmClient.ibc.channel.packetReceipt(
+          packet.destPortAddress,
+          packet.destChannelId,
+          Number(packet.sequence)
+        );
 
-      if (packetReceipt.received) {
-        packet.state = PacketStates.POLY_RECV;
-        unprocessedPacketKeys.delete(key);
+        if (packetReceipt.received) {
+          packet.state = PacketStates.POLY_RECV;
+          unprocessedPacketKeys.delete(key);
+        }
+      } catch (error) {
+        console.error(`Error processing packet with key ${key}:`, error);
       }
-    } catch (error) {
-      console.error(`Error processing packet with key ${key}:`, error);
-    }
-  }));
+    })
+  );
 
   const response: Packet[] = [];
   Object.keys(packets).forEach((key) => {
     response.push(packets[key]);
   });
 
-  cache.set("allPackets", response, getCacheTTL());
   return NextResponse.json(response);
 }
--- app/api/utils/provider-cache.ts
+++ app/api/utils/provider-cache.ts
@@ -6,16 +6,21 @@ export class CachingJsonRpcProvider extends ethers.JsonRpcProvider {
 
   constructor(url: string, network?: ethers.Networkish) {
     super(url, network);
-    this.cache = flatCache.load('ethCache', "/tmp");
+    this.cache = flatCache.load('ethCache', '/tmp');
   }
 
-  private async fetchDataWithCache<T>(cacheKey: string, fetchFunction: () => Promise<T>): Promise<T> {
+  private async fetchDataWithCache<T>(
+    cacheKey: string,
+    fetchFunction: () => Promise<T>
+  ): Promise<T> {
     // Check if the data is already in the cache
     const cachedData = this.cache.getKey(cacheKey);
     if (cachedData) {
+      console.log('CACHE HIT: ', cacheKey);
       return cachedData;
     }
 
+    console.log('CACHE MISS: ', cacheKey);
     // Fetch data from the external service
     const data = await fetchFunction();
 
@@ -26,7 +31,9 @@ export class CachingJsonRpcProvider extends ethers.JsonRpcProvider {
   }
 
   // Override the method to add caching
-  async getBlock(blockHashOrBlockTag: string | number): Promise<ethers.Block | null> {
+  async getBlock(
+    blockHashOrBlockTag: string | number
+  ): Promise<ethers.Block | null> {
     return await this.fetchDataWithCache(
       `${this._network.chainId}_block_${blockHashOrBlockTag}`,
       async () => await super.getBlock(blockHashOrBlockTag)
@@ -35,10 +42,19 @@ export class CachingJsonRpcProvider extends ethers.JsonRpcProvider {
 
   // Override other methods as needed
   // Example: Override getTransactionReceipt method
-  async getTransactionReceipt(transactionHash: string): Promise<ethers.TransactionReceipt | null> {
+  async getTransactionReceipt(
+    transactionHash: string
+  ): Promise<ethers.TransactionReceipt | null> {
     return await this.fetchDataWithCache(
       `${this._network.chainId}_txReceipt_${transactionHash}`,
       async () => await super.getTransactionReceipt(transactionHash)
+    );
+  }
+
+  async getLogs(filter: ethers.Filter): Promise<Array<ethers.Log>> {
+    return await this.fetchDataWithCache(
+      `${this._network.chainId}_logs_${filter.address}_${filter.fromBlock}_${filter.toBlock}`,
+      async () => await super.getLogs(filter)
     );
   }
 }
