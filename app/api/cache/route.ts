8d2a0e41fee0c04c86bb00e550dec4e90d04e65b
Inkvi <alex@polymerlabs.org>
2024-03-11 12:24:05 -0700

    Add a route to refresh packets cache

---
+++ app/api/cache/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getCacheTTL, SimpleCache } from '@/api/utils/cosmos';
+import { getPackets } from '@/api/packets/route';
+
+export async function GET(request: NextRequest) {
+  const TOKEN = process.env.TOKEN;
+
+  if (request.headers.get('Authorization') !== `Bearer ${TOKEN}`) {
+    return NextResponse.error();
+  }
+
+  const cache = SimpleCache.getInstance();
+  try {
+    const packets = await getPackets('1', 'latest');
+    cache.set('allPackets', packets, getCacheTTL());
+    return NextResponse.json(packets);
+  } catch (e) {
+    return NextResponse.error();
+  }
+}
