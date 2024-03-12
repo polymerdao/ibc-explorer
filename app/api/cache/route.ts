818e2fc36d8f8d1a55ccfdaf8718aebf637cd643
Inkvi <alex@polymerlabs.org>
2024-03-11 19:50:18 -0700

    Disabled TOKEN

--- app/api/cache/route.ts
+++ app/api/cache/route.ts
@@ -3,12 +3,12 @@ import { getCacheTTL, SimpleCache } from '@/api/utils/cosmos';
 import { getPackets } from '@/api/packets/route';
 
 export async function GET(request: NextRequest) {
-  const TOKEN = process.env.TOKEN;
-
-  if (request.headers.get('Authorization') !== `Bearer ${TOKEN}`) {
-    console.error('Unauthorized request');
-    return NextResponse.error();
-  }
+  // const TOKEN = process.env.TOKEN;
+  //
+  // if (request.headers.get('Authorization') !== `Bearer ${TOKEN}`) {
+  //   console.error('Unauthorized request');
+  //   return NextResponse.error();
+  // }
 
   const cache = SimpleCache.getInstance();
   try {
