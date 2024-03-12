bd9866848707519938c68804ab0e2b5546aee053
Inkvi <alex@polymerlabs.org>
2024-03-12 09:48:02 -0700

    Require AUTH_TOKEN

--- app/api/cache/route.ts
+++ app/api/cache/route.ts
@@ -5,12 +5,12 @@ import { SimpleCache } from '@/api/utils/cache';
 export const dynamic = 'force-dynamic'; // defaults to auto
 
 export async function GET(request: NextRequest) {
-  // const TOKEN = process.env.TOKEN;
-  //
-  // if (request.headers.get('Authorization') !== `Bearer ${TOKEN}`) {
-  //   console.error('Unauthorized request');
-  //   return NextResponse.error();
-  // }
+  const AUTH_TOKEN = process.env.AUTH_TOKEN;
+
+  if (request.headers.get('Authorization') !== `Bearer ${AUTH_TOKEN}`) {
+    console.error('Unauthorized request');
+    return NextResponse.error();
+  }
 
   const cache = SimpleCache.getInstance();
   try {
