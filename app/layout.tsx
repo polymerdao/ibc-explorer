946bf3ee60f79c9b8de4fdf75838e6a700237fb9
mvpoyatt <mpoyatt@gmail.com>
2024-03-14 09:12:40 -0400

    Simplified setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         <main>
-          <div className="relative flex place-items-center justify-end overflow-none z-[-1] blur-[2.2rem] before:content-[''] before:absolute before:h-[6rem] before:w-[60rem] before:bg-white before:dark:bg-slate-900 before:rounded-[50%] origin-left rotate-[23deg] top-auto right-auto md:top-[-6rem] md:right-[-4rem] lg:top-[-6rem] lg:right-[-5rem] xl:top-[-8rem] xl:right-[-6rem] 2xl:top-[-8rem] 2xl:right-[-14rem]">
-          </div>
           {children}
         </main>
       </body>
