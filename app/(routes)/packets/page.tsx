5319cd7494c9413ef6eb00e60035fccd007cb96a
mvpoyatt <mpoyatt@gmail.com>
2024-07-11 11:00:28 -0400

    Stashing progress

--- app/(routes)/packets/page.tsx
+++ app/(routes)/packets/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Fragment } from 'react';
 import {
   VisibilityState,
   createColumnHelper,
@@ -16,9 +16,14 @@ import { Modal } from 'components/modal';
 import { Packet } from 'utils/types/packet';
 import { PacketDetails } from './packet-details';
 import { stateToString } from 'utils/types/packet';
+import { CHAIN_CONFIGS } from 'utils/chains/configs';
+import { Select } from 'components/select';
 import { StateCell } from 'components/state-cell';
 import { ChainCell, Arrow } from 'components/chain-cell';
 import { shortenHex } from 'components/format-strings';
+import { classNames } from 'utils/functions';
+import { FiChevronDown } from 'react-icons/fi';
+import { Transition, Popover } from '@headlessui/react';
 
 const columnHelper = createColumnHelper<Packet>();
 const columns = [
@@ -118,9 +123,23 @@ const columns = [
   })
 ];
 
+interface state {
+  value: string;
+  label: string;
+  selected: boolean;
+}
+
 export default function Packets() {
   const [packets, setPackets] = useState<Packet[]>([]);
   const [searchValue, setSearchValue] = useState<string>('');
+  const [showFilters, setShowFilters] = useState(false);
+  const [srcFilter, setSrcFilter] = useState<string>('');
+  const [destFilter, setDestFilter] = useState<string>('');
+  const [stateFilter, setStateFilter] = useState<Array<state>>([
+    { value: 'SENT', label: 'Relaying', selected: false },
+    { value: 'RECV,WRITE_ACK', label: 'Confirming', selected: false },
+    { value: 'ACK', label: 'Delivered', selected: false }
+  ]);
   const [packetSearch, setPacketSearch] = useState(false);
   const [foundPacket, setFoundPacket] = useState<Packet | null>(null);
   const [resType, setResType] = useState<string>('all');
@@ -174,7 +193,16 @@ export default function Packets() {
     setSearchLoading(true);
     setFoundPacket(null);
     setPacketSearch(true);
-    fetch(`/api/packets?searchValue=${searchValue}`, { signal: controller.signal })
+    let states = '';
+    for (const state of stateFilter) {
+      if (state.selected) {
+        states += state.value + ',';
+      }
+    }
+    fetch(
+      `/api/packets?searchValue=${searchValue}&src=${srcFilter}&dest=${destFilter}&states=${states}`,
+      { signal: controller.signal }
+    )
       .then(res => {
         if (!res.ok) {
           setErrorMessage(res.statusText);
@@ -271,6 +299,27 @@ export default function Packets() {
             onClick={() => searchPackets()}>
             Search
           </button>
+          <button
+            className="ml-3.5 pt-0.5 pb-1.5 px-2 grid justify-items-center items-center"
+            onClick={() => setShowFilters(!showFilters)}>
+            <div className={classNames(
+              showFilters
+              ? 'translate-y-5'
+              : '',
+              'relative h-0.5 w-7 mt-1 bg-primary-500 transition-transform duration-200 ease-in-out'
+            )}>
+            </div>
+            <div
+              className='h-0.5 w-4 bg-primary-500'>
+            </div>
+            <div className={classNames(
+              showFilters
+              ? '-translate-y-5'
+              : '',
+              'relative h-0.5 w-1.5 bg-primary-500 transition-transform duration-200 ease-in-out'
+            )}>
+            </div>
+          </button>
         </div>
         <button 
           onClick={() => {
@@ -280,6 +329,87 @@ export default function Packets() {
           className="btn btn-accent">
           Reload
         </button>
+      </div>
+
+      <div 
+        className={classNames(
+          showFilters
+          ? 'h-8'
+          : 'h-0'
+          , 'w-full mt-4 transition-all ease-in-out duration-200'
+        )}>
+        <div
+          className={classNames(
+            showFilters
+            ? 'opacity-100'
+            : 'opacity-0'
+            , 'relative flex flex-row space-x-2 items-center w-full mx-auto transition-all ease-in-out delay-100 duration-100'
+          )}>
+          <Popover>
+            {({ open }) => (<>
+              <Popover.Button className="inpt w-28 flex flex-row justify-between items-center h-8 pl-[9px] pr-[0.4rem] text-fg-light dark:text-fg-dark transition east-in-out duration-200 cursor-default">
+                State
+                <FiChevronDown className={classNames(
+                  open
+                  ? 'scale-y-[-1]'
+                  : ''
+                  , 'mt-[1px] transition ease-in-out duration-200'
+                )}/>
+              </Popover.Button>
+              <Transition
+                as={Fragment}
+                enter="ease-out duration-200"
+                enterFrom="transform scale-95 opacity-0 -translate-y-6"
+                enterTo="transform scale-100 opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0">
+                <Popover.Panel className="absolute z-20 mt-2 left-0">
+                  <div className="bg-bg-light dark:bg-bg-dark pl-6 pr-9 py-4 border-[0.5px] rounded-md border-slate-500">
+                    {stateFilter.map(state => { return (
+                      <div key={state.value} className="py-[0.17rem]">
+                        <label>
+                          <input
+                            className="appearance-none border border-slate-500 bg-transparent rounded-lg w-3 h-3 mr-3 transition-colors ease-in-out duration-150
+                              checked:bg-emerald-500 checked:border-transparent"
+                            {...{
+                              type: 'checkbox',
+                              checked: state.selected,
+                              onChange: () => {state.selected = !state.selected; setStateFilter([...stateFilter])}
+                            }}
+                          />
+                          {state.label}
+                        </label>
+                      </div>
+                      )
+                    })}
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </>)}
+          </Popover>
+
+          <Select 
+            options={
+              [{ value: '', label: 'Source' }, 
+              ...Object.entries(CHAIN_CONFIGS).map(([key, value]) => ({ value: key, label: value.display }))]
+            }
+            onChange={value => setSrcFilter(value as string)}
+            containerClassName="w-28"
+            buttonClassName="inpt h-8 pl-[9px] cursor-default"
+            dropdownClassName="bg-bg-light dark:bg-bg-dark"
+          />
+          <Select 
+            options={
+              [{ value: '', label: 'Dest' }, 
+              ...Object.entries(CHAIN_CONFIGS).map(([key, value]) => ({ value: key, label: value.display }))]
+            }
+            onChange={value => setDestFilter(value as string)}
+            containerClassName="w-28"
+            buttonClassName="inpt h-8 pl-[9px] cursor-default"
+            dropdownClassName="bg-bg-light dark:bg-bg-dark"
+          />
+        </div>
       </div>
 
       <IbcTable {...{table, loading, rowDetails: PacketDetails}} />
