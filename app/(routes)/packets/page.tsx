9264cb032941e292ba906f3bb590426d631c7b0a
mvpoyatt <mpoyatt@gmail.com>
2024-07-22 19:27:09 -0400

    More ways to remove caching

--- app/(routes)/packets/page.tsx
+++ app/(routes)/packets/page.tsx
@@ -194,7 +194,7 @@ export default function Packets() {
     const offset = (pageNumber - 1) * PAGE_SIZE;
 
     fetch(
-      `/api/packets?searchValue=${searchValue}&src=${srcFilter}&dest=${destFilter}&states=${states}&limit=${PAGE_SIZE}&offset=${offset}`,
+      `/api/packets?searchValue=${searchValue}&src=${srcFilter}&dest=${destFilter}&states=${states}&limit=${PAGE_SIZE}&offset=${offset}&timestamp=${Date.now()}`,
       {
         cache: 'no-store',
         signal: controller.signal
