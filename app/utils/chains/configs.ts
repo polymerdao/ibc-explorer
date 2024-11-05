2f1fb096952d0de6728a9d905c06ccc7ef50d46b
Inkvi <alex@polymerlabs.org>
2024-11-05 12:06:12 -0800

    Add Bob and Mode chains

--- app/utils/chains/configs.ts
+++ app/utils/chains/configs.ts
@@ -1,16 +1,22 @@
 import { Chain } from 'utils/types/chain';
-import { OptimismIcon, BaseIcon } from './icons';
+import { OptimismIcon, BaseIcon, ModeIcon, BobIcon } from './icons';
 
-export type CHAIN = 'optimism' | 'base';
+export type CHAIN = 'optimism' | 'base' | 'mode' | 'bob';
 
 let opDispatcher = process.env.DISPATCHER_ADDRESS_OPTIMISM!;
 let baseDispatcher = process.env.DISPATCHER_ADDRESS_BASE!;
+let modeDispatcher = process.env.DISPATCHER_ADDRESS_MODE!;
+let bobDispatcher = process.env.DISPATCHER_ADDRESS_BOB!;
 
 let opClientName = process.env.OPTIMISM_CLIENT_NAME!;
 let baseClientName = process.env.BASE_CLIENT_NAME!;
+let modeClientName = process.env.MODE_CLIENT_NAME!;
+let bobClientName = process.env.BOB_CLIENT_NAME!;
 
 let optimismRPC = process.env.OPTIMISM_RPC!;
 let baseRPC = process.env.BASE_RPC!;
+let modeRPC = process.env.MODE_RPC!;
+let bobRPC = process.env.BOB_RPC!;
 
 export const CHAIN_CONFIGS: {
   [key in CHAIN]: Chain;
@@ -30,7 +36,23 @@ export const CHAIN_CONFIGS: {
     clients: [baseClientName],
     blockTime: 2,
     icon: BaseIcon,
-  }
+  },
+  mode: {
+    display: 'Mode',
+    rpc: modeRPC,
+    dispatchers: [modeDispatcher],
+    clients: [modeClientName],
+    blockTime: 2,
+    icon: ModeIcon,
+  },
+  bob: {
+    display: 'Bob',
+    rpc: bobRPC,
+    dispatchers: [bobDispatcher],
+    clients: [bobClientName],
+    blockTime: 2,
+    icon: BobIcon,
+  },
 };
 
 export function clientToDisplay(client: string) {
--- app/utils/chains/id-maps.ts
+++ app/utils/chains/id-maps.ts
@@ -15,16 +15,20 @@ export const getChainId = (chainName: string, env: string): number => {
   }
 
   return chainMap[chainName as ChainName];
-}
+};
 
-export type ChainName = 'optimism' | 'base';
+export type ChainName = 'optimism' | 'base' | 'mode' | 'bob';
 
 export const mainnetIdMap: Record<ChainName, number> = {
-   'optimism': 10,
-   'base': 8453,
- }
+  'optimism': 10,
+  'base': 8453,
+  'mode': 34443,
+  'bob': 60808
+};
 
 export const sepoliaIdMap: Record<ChainName, number> = {
   'optimism': 11155420,
   'base': 84532,
-}
+  'mode': 919,
+  'bob': 808813
+};
